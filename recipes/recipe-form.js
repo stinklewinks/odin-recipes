74e0c247e8e04b4500e8ad3eaa2aca1b017028f2
Drew <drew@mybrandhero.co>
2022-11-24 22:30:01 -0600

    Added css structure and todo strucutre

--- recipes/recipe-form.js
+++ recipes/recipe-form.js
@@ -1,8 +1,14 @@
+
+//TODO: model a recipe database
 const recipes = {
     "Lasagna": {
+        author: "Lisa Winkles",
         ingredients: ['Canned Tomatoes', 'Onions', 'Ground Meat'],
+        prepTime: 45,
         cookTime: 25,
+        steps: [],
         description: "This is a cool dish.",
+        rating: 5,
     }
 }
 
@@ -15,6 +21,8 @@ const outputForm = document.getElementById('output');
 
 const addToRecipes = (e) => {
     e.preventDefault();
+
+    // TODO: Test to see if these variables are an extra step
     let recipeNameData = recipeName.value;
     let recipeAuthorData = author.value;
     let recipeDescData = recipeDesc.value;
