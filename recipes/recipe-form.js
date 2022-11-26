bc420e42366605a7fc26c887f825b0c2c5aba094
Drew <drew@mybrandhero.co>
2022-11-25 19:39:02 -0600

    Added an Add Step function and cleaned up script

--- recipes/recipe-form.js
+++ recipes/recipe-form.js
@@ -16,23 +16,36 @@ const form = document.getElementById('add-recipe');
 const recipeName = document.getElementById('recipe-name');
 const author = document.getElementById('author');
 const recipeDesc = document.getElementById('recipe-description');
+const stepsContainer = document.getElementById('steps-container');
+const addStepButton = document.getElementById('add-step');
+const steps = document.querySelectorAll('.step');
+
 
 const outputForm = document.getElementById('output');
 
+const addStep = () => {
+    let step = document.createElement('input');
+    let pageBreak = document.createElement('br');
+    step.className = `step`;
+    stepsContainer.appendChild(step);
+    stepsContainer.appendChild(pageBreak);
+}
+
 const addToRecipes = (e) => {
     e.preventDefault();
-
-    // TODO: Test to see if these variables are an extra step
     let recipeNameData = recipeName.value;
     let recipeAuthorData = author.value;
     let recipeDescData = recipeDesc.value;
+    let stepsData = [steps[0].value, steps[1].value, steps[2].value];
 
     recipes[recipeNameData] = {
         author: recipeAuthorData,
-        description: recipeDescData
+        description: recipeDescData,
+        steps: [stepsData[0], stepsData[1], stepsData[2]]
     };
     console.log(recipes);
     form.reset();
 }
 
 form.addEventListener('submit', addToRecipes);
+addStepButton.addEventListener('click', addStep);
